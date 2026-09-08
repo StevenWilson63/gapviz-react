f7c404053f1eea72d40d9b8a6952d11b950458d3
Steven Wilson <stevenwilson@gsjproducts.co.uk>
2026-09-08 15:37:50 +0100

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Splash from "./components/Splash/Splash";
+
+export default function App() {
+  return <Splash />;
+}
---
+++ src/components/Splash/Splash.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Splash.css";
+
+export default function Splash() {
+  return (
+    <div className="splash-container">
+      <h1 className="splash-title">gapviz</h1>
+      <p className="splash-tagline">unlock the song and story</p>
+    </div>
+  );
+}
