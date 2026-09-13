a61b83f44d0f389f30daa99e786eb06c0ffdf9dc
Steven Wilson <stevenwilson@gsjproducts.co.uk>
2026-09-13 14:42:57 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,14 @@ import { UserProvider } from "./context/UserContext";
 import Home from "./pages/home/home";
 import Profile from "./pages/profile/Profile";
 
-// If these screens exist, they will load.
-// If not, you can add them later.
-
 export default function App() {
   return (
     <UserProvider>
       <BrowserRouter>
         <Routes>
-          {/* Default route */}
           <Route path="/" element={<Navigate to="/home" replace />} />
-
-          {/* Main screens */}
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
-
-          {/* Additional screens */}
-
-          {/* Fallback */}
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
       </BrowserRouter>
