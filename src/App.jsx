07c025962933bb7b1fc39cc78864ee65026553e7
Steven Wilson <stevenwilson@gsjproducts.co.uk>
2026-09-13 14:38:46 +0100

    cloudflare fix - remove missing imports

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ import Profile from "./pages/profile/Profile";
 
 // If these screens exist, they will load.
 // If not, you can add them later.
-// import DJ from "./pages/dj/DJ";
-// import Identify from "./pages/identify/Identify";
-// import Library from "./pages/library/Library";
-// import SongStory from "./pages/songstory/SongStory";
-// import IdentifySummary from "./pages/identify/IdentifySummary";
 
 export default function App() {
   return (
@@ -27,11 +22,6 @@ export default function App() {
           <Route path="/profile" element={<Profile />} />
 
           {/* Additional screens */}
-          {/* <Route path="/dj" element={<DJ />} /> */}
-{/* <Route path="/identify" element={<Identify />} /> */}
-{/* <Route path="/library" element={<Library />} /> */}
-{/* <Route path="/songstory" element={<SongStory />} /> */}
-{/* <Route path="/identify-summary" element={<IdentifySummary />} /> */}
 
           {/* Fallback */}
           <Route path="*" element={<Navigate to="/home" replace />} />
