063df0957c2fc6d5262411af976182fc5255d600
Steven Wilson <stevenwilson@gsjproducts.co.uk>
2026-09-15 17:17:48 +0100

    recreate profile folder cleanly

---
+++ src/pages/profile/profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <h1>Profile</h1>
+      <p>This is your profile screen.</p>
+    </div>
+  );
+}
